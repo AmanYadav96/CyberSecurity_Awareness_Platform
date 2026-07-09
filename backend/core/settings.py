5fadcd3d6a0193d343de3c112f491e23e5747e08
Shreehari615 <shreeharisvbhat2005@gmail.com>
2026-07-09 13:40:19 +0530

    tls to ssl

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -195,7 +195,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = "cybersecurityapplication@gmail.com"
 EMAIL_HOST_PASSWORD = "crrclvoocgdxedvc"
